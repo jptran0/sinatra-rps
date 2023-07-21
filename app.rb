1e5528208111ac0afe53b40083dac0b900fa1b25
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-07-21 17:59:25 +0000

    complete rps routes and views

--- app.rb
+++ app.rb
@@ -4,3 +4,21 @@ require "sinatra/reloader"
 get("/") do
   erb(:home)
 end
+
+get("/rock") do
+  @comp_choice = ["rock", "paper", "scissors"].sample
+
+  erb(:rock)
+end
+
+get("/paper") do
+  @comp_choice = ["rock", "paper", "scissors"].sample
+
+  erb(:paper)
+end
+
+get("/scissors") do
+  @comp_choice = ["rock", "paper", "scissors"].sample
+  
+  erb(:scissors)
+end
